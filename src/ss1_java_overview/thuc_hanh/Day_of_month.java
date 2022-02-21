c8c69df874887f97bdb9296902f38731d335abf4
huuphucdn94 <huuphucdn94@gmail.com>
2022-02-21 20:10:32 +0700

    Module 2

---
+++ src/ss1_java_overview/thuc_hanh/Day_of_month.java
@@ -0,0 +1,42 @@
+package ss1_java_overview.thuc_hanh;
+
+import java.util.Scanner;
+
+public class Day_of_month {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which month that you want to count days? ");
+        int month = scanner.nextInt();
+        String datesInMonth;
+        switch  (month) {
+            case 2:
+                datesInMonth = " 28 or 29";
+//                System.out.print("The month '2' has 28 or 29 days!");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                datesInMonth = "31";
+//                System.out.print("The month " + month + " has 31 days!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                datesInMonth = "30";
+//                System.out.print("The month " + month + " has 30 days");
+                break;
+            default:
+                datesInMonth ="";
+//                System.out.print("Invalid input");
+                break;
+        }
+        if (!datesInMonth.equals("")) {
+            System.out.print("The month '%d' has '%s' days", month, datesInMonth);
+        }
+    }
+}
---
+++ src/ss1_java_overview/thuc_hanh/Declare_and_use_variables.java
@@ -0,0 +1,16 @@
+package ss1_java_overview.thuc_hanh;
+import java.util.Scanner;
+public class Declare_and_use_variables {
+    public static void main(String[] args) {
+        float wight;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Wight: ");
+        wight = scanner.nextFloat();
+        System.out.println("Enter Height: ");
+        height = scanner.nextFloat();
+        float area = wight * height;
+        System.out.println("Area is: " + area);
+
+    }
+}
